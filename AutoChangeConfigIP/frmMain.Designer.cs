2addfd9f60c8e2d5f5c22329b6bf9b57b9dda8db
Edward <edward_song@yeah.net>
2018-11-08 09:22:54 +0800

    ui ok

--- AutoChangeConfigIP/frmMain.Designer.cs
+++ AutoChangeConfigIP/frmMain.Designer.cs
@@ -28,21 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtServerName = new System.Windows.Forms.TextBox();
+            this.txtConfigPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstMsg = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtServerName);
+            this.groupBox1.Controls.Add(this.txtConfigPath);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(414, 103);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(328, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 30);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "停止监听";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "开始监听";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtServerName
+            // 
+            this.txtServerName.Location = new System.Drawing.Point(93, 62);
+            this.txtServerName.Name = "txtServerName";
+            this.txtServerName.Size = new System.Drawing.Size(229, 21);
+            this.txtServerName.TabIndex = 3;
+            this.txtServerName.TextChanged += new System.EventHandler(this.txtServerName_TextChanged);
+            // 
+            // txtConfigPath
+            // 
+            this.txtConfigPath.Location = new System.Drawing.Point(93, 25);
+            this.txtConfigPath.Name = "txtConfigPath";
+            this.txtConfigPath.Size = new System.Drawing.Size(229, 21);
+            this.txtConfigPath.TabIndex = 2;
+            this.txtConfigPath.TextChanged += new System.EventHandler(this.txtConfigPath_TextChanged);
+            this.txtConfigPath.DoubleClick += new System.EventHandler(this.txtConfigPath_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "服务器电脑名";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "配置档地址";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstMsg);
+            this.groupBox2.Location = new System.Drawing.Point(12, 130);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(414, 366);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // lstMsg
+            // 
+            this.lstMsg.FormattingEnabled = true;
+            this.lstMsg.ItemHeight = 12;
+            this.lstMsg.Location = new System.Drawing.Point(6, 20);
+            this.lstMsg.Name = "lstMsg";
+            this.lstMsg.Size = new System.Drawing.Size(402, 340);
+            this.lstMsg.TabIndex = 0;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(649, 344);
+            this.ClientSize = new System.Drawing.Size(443, 508);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtServerName;
+        private System.Windows.Forms.TextBox txtConfigPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstMsg;
     }
 }
 
--- AutoChangeConfigIP/frmMain.cs
+++ AutoChangeConfigIP/frmMain.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using Edward;
 
 namespace AutoChangeConfigIP
 {
@@ -13,6 +15,117 @@ namespace AutoChangeConfigIP
         public frmMain()
         {
             InitializeComponent();
+        }
+
+
+        private static string AppFolder = Application.StartupPath + @"\AutoChangeConfigIP";
+        private static string AppLog = AppFolder + @"\Log";
+        private static string AppConfig = AppFolder + @"\SysConfig.ini";
+        private static string ConfigPath = @"D:\RecordColl\RecordCollCfg.ini";
+        private static string ServerName = "";
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="textbox"></param>
+        private void OpenFile(TextBox textbox)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "配置文件|*.ini|全部文件|*.*";
+            if (open.ShowDialog() == DialogResult.OK)
+                textbox.Text  = open.FileName;
+
+        }
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CreateFolder()
+        {
+            if (!Directory.Exists(AppFolder))
+                Directory.CreateDirectory(AppFolder);
+            if (!Directory.Exists(AppLog))
+                Directory.CreateDirectory(AppLog);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void Init()
+        {
+            this.Text = "自动更改采集站服务器IP,Ver:" + Application.ProductVersion;
+            CreateFolder();
+            CreateIni();
+            ReadIni();
+            LoadData();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CreateIni()
+        {
+            if (!File.Exists(AppConfig))
+            {
+                IniFile.CreateIniFile(AppConfig);
+                IniFile.iniFilePathValue = AppConfig;
+                IniFile.IniWriteValue("SysConfig", "ConfigPath", ConfigPath, AppConfig);
+                IniFile.IniWriteValue("SysConfig", "ServerName", ServerName, AppConfig);
+            }
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void ReadIni()
+        {
+           // IniFile.iniFilePathValue = AppConfig;
+            if (File.Exists(AppConfig))
+            {
+                ConfigPath = IniFile.IniReadValue("SysConfig", "ConfigPath", AppConfig);
+                ServerName = IniFile.IniReadValue("SysConfig", "ServerName", AppConfig);
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void LoadData()
+        {
+            txtConfigPath.Text = ConfigPath;
+            txtServerName.Text = ServerName;
+
+        }
+
+        private void txtConfigPath_DoubleClick(object sender, EventArgs e)
+        {
+            OpenFile(txtConfigPath);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+        private void txtConfigPath_TextChanged(object sender, EventArgs e)
+        {
+            ConfigPath = txtConfigPath.Text.Trim();
+            IniFile.IniWriteValue("SysConfig", "ConfigPath", ConfigPath, AppConfig);
+        }
+
+        private void txtServerName_TextChanged(object sender, EventArgs e)
+        {
+            ServerName = txtServerName.Text.Trim();
+            ServerName = IniFile.IniReadValue("SysConfig", "ServerName", AppConfig);
         }
     }
 }
